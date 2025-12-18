1c296fec72de656185795a55956555f9f195d868
poonpak <42527560+poonpak@users.noreply.github.com>
2025-12-18 11:34:00 +0700

    Update main.go

--- app/main.go
+++ app/main.go
@@ -1,39 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 type Simple struct {
-	Name        string
-	Description string
-	Url         string
+    Name        string
+    Description string
+    Url         string
 }
 
-// func handler(w http.ResponseWriter, r *http.Request) {
-//     simple := Simple{"Hello", "World", r.Host}
-
-//     jsonOutput, _ := json.Marshal(simple)
-
-//     w.Header().Set("Content-Type", "application/json")
-
-//	    fmt.Fprintln(w, string(jsonOutput))
-//	}
-//
-// ...
 func SimpleFactory(host string) Simple {
-	return Simple{"Hello", "World", host}
+    return Simple{"Goodbye", "Students", host}
 }
 
-// ...
 func handler(w http.ResponseWriter, r *http.Request) {
-	SimpleFactory(r.Host)
+    simple := SimpleFactory(r.Host)
+
+    jsonOutput, _ := json.Marshal(simple)
+
+    w.Header().Set("Content-Type", "application/json")
+
+    fmt.Fprintln(w, string(jsonOutput))
 }
 
 func main() {
-	fmt.Println("Server started on port 4444")
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":4444", nil))
+    fmt.Println("Server started on port 4444")
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":4444", nil))
 }
